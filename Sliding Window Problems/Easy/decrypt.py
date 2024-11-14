164927d1f1c12afb76701b67aac44b9d760c6646
acheampe <acheampe@oregonstate.edu>
2024-11-14 12:49:05 -0500

    Completed positive k

--- Sliding Window Problems/Easy/decrypt.py
+++ Sliding Window Problems/Easy/decrypt.py
@@ -1,4 +1,35 @@
 from typing import List
+## 1652. Defuse the Bomb
 
 class Solution:
     def decrypt(self, code: List[int], k: int) -> List[int]:
+        """
+        Decrypts current window and places decrypted value in previous index
+        """
+
+        # Edge case
+        if k > len(code):
+            print("Invalid")
+            return -1 
+        
+        # initialize decryptArr to 0s
+        decryptCode = [ 0 for i in range(len(code))]
+
+        # Calc. first window and initialize to previous index
+        currSum = sum(code[:k])
+
+        # Set up a wrap around loop
+        for i in range(len(code)):
+            if i == 0:
+                decryptCode[len(code) - 1] = currSum
+            
+            else:
+                # subtracting init window index and adding last window index
+                currSum = currSum - (code[(i - 1) % len(code)]) + (code[(k + i - 1) % len(code)])
+                decryptCode[i - 1] = currSum
+        
+        return decryptCode
+    
+sol = Solution()
+first_dec = sol.decrypt([5,7,1,4], 3)
+print(first_dec)
