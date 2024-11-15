9aa92889b72dcae4e51bc3dee3b1bcc3338e1574
acheampe <acheampe@oregonstate.edu>
2024-11-15 11:04:45 -0500

    implemented neg variant

--- Sliding Window Problems/Easy/decrypt.py
+++ Sliding Window Problems/Easy/decrypt.py
@@ -4,32 +4,41 @@ from typing import List
 class Solution:
     def decrypt(self, code: List[int], k: int) -> List[int]:
         """
-        Decrypts current window and places decrypted value in previous index
+        Decrypt current window and places decrypted value in previous index
         """
 
-        # Edge case
-        if k > len(code):
-            print("Invalid")
-            return -1 
-        
-        # initialize decryptArr to 0s
-        decryptCode = [ 0 for i in range(len(code))]
+        arrlen = len(code)
+        # initialize decryptArr
+        decryptCode = [ 0 for i in range(arrlen)]
 
-        # Calc. first window and initialize to previous index
+        # Calc. first window size
         currSum = sum(code[:k])
 
-        # Set up a wrap around loop
-        for i in range(len(code)):
-            if i == 0:
-                decryptCode[len(code) - 1] = currSum
+        # Set up wrap around loop
+        for i in range(arrlen):
+
+            if k < 0: # if neg
+                if i == 0:
+                    decryptCode[abs(k)] = currSum
+                else:
+                    currSum = currSum - code[(i - 1) % arrlen] + code[(i + 1) % arrlen]
+                    decryptCode[(i + k) % arrlen] = currSum
             
             else:
-                # subtracting init window index and adding last window index
-                currSum = currSum - (code[(i - 1) % len(code)]) + (code[(k + i - 1) % len(code)])
-                decryptCode[i - 1] = currSum
+                if i == 0:
+                    decryptCode[arrlen - 1] = currSum
+                
+                else:
+                    # subtracting inti window index and adding last window index from currSum
+                    currSum = currSum - code[(i - 1) % arrlen] + code[(k + i - 1) % arrlen]
+                    decryptCode[i-1] = currSum
         
         return decryptCode
+
+        
     
 sol = Solution()
 first_dec = sol.decrypt([5,7,1,4], 3)
 print(first_dec)
+sec_dec = sol.decrypt([2,4,9,3], -2)
+print(sec_dec)
