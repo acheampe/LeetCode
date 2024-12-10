62dc5a6c7c99321c78a41991018d62abe2f022dc
acheampe <acheampe@oregonstate.edu>
2024-12-09 19:03:14 -0500

    Updated

--- Sliding Window Problems/Easy/findMaxAverage.py
+++ Sliding Window Problems/Easy/findMaxAverage.py
@@ -1,37 +1,70 @@
 from typing import List
 
+# class Solution:
+#     def findMaxAverage(self, nums: List[int], k: int) -> float:
+#         """
+#         Return max average of k window in an array
+#         """
+        
+#         arrLen = len(nums)
+
+#         if k > arrLen:
+#             print(f'Invalid: {k} value greater than length of array --> {nums}')
+        
+#         # Calc. val of current window
+#         currVal = sum(nums[i] for i in range(0, k, 1))
+
+#         maxVal = currVal
+
+#         # iterate using window to find max average
+#         for i in range (1, arrLen - k + 1):
+            
+#             # calc next iterative sum 
+#             dropVal = nums[i - 1]
+#             addVal = nums[k + i -1]
+
+#             currVal -= dropVal
+#             currVal += addVal
+
+#             maxVal = max(currVal, maxVal)
+        
+#         return maxVal / k 
+
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
         """
-        Return max average of k window in an array
+        return the max average of the window within arr
         """
-        
+
+        # Initialize length of Arr
         arrLen = len(nums)
 
+        # Edge case:
         if k > arrLen:
-            print(f'Invalid: {k} value greater than length of array --> {nums}')
+            print("Invalid input")
+            return -1
         
-        # Calc. val of current window
-        currVal = sum(nums[i] for i in range(0, k, 1))
-
+        # Initialize Vals
+        currVal = sum(nums[:k])
         maxVal = currVal
 
-        # iterate using window to find max average
-        for i in range (1, arrLen - k + 1):
-            
-            # calc next iterative sum 
-            dropVal = nums[i - 1]
-            addVal = nums[k + i -1]
+        # Iterate to find max
+        for i in range(1, arrLen - k + 1):
 
-            currVal -= dropVal
-            currVal += addVal
+            dropVal = nums[i - 1]
+            addVal = nums[(i - 1) + k]
+            
+            currVal = currVal - dropVal + addVal
 
             maxVal = max(currVal, maxVal)
+
+        # return average
+        return maxVal / k
+
         
-        return maxVal / k 
 
 sol = Solution()
-first = sol.findMaxAverage([2,4,6,8,10], 3)
+first = sol.findMaxAverage([0,1,1,3,3], 4)
 print(first)
 
 
