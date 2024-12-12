d6d4b4806081806b86272e8153b5f5571a2902ee
acheampe <acheampe@oregonstate.edu>
2024-12-12 18:54:03 -0500

    draft fruitbasket

---
+++ Sliding Window Problems/Medium/fruitIntoBasket.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        pass
+
+
+sol = Solution()
+print(sol.totalFruit([1,2,1])) #Expected: 3
+# print(sol.totalFruit([0,1,2,2])) #Expected: 3
