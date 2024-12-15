f3e08ae4db19cfc3e1a01fc96cc0d5a774a51c04
acheampe <acheampe@oregonstate.edu>
2024-12-14 19:48:06 -0500

    non optimized solution

--- 1_Sliding Window Problems/Medium/s2_fruitIntoBasket.py
+++ 1_Sliding Window Problems/Medium/s2_fruitIntoBasket.py
@@ -6,54 +6,51 @@ class Solution:
         return the max number of subarr length of tree of two types
         """
 
-        # Initial Variables
-        typeCounter = 0  # track unique counts
+        fruitTree = {}
+        currRowLength, maxRowLength = 0, 0
+        typeTracker = 0 # monitors length of dictionary
         leftIndex, rightIndex = 0, 0
-        fruitTracker = set()
-        maxRowLength = 0
-        currRowLength = 0
 
         if len(fruits) == 1:
-            return 1 # only one option 
-        
+            return 1
+
         while rightIndex < len(fruits):
-            #track fruit type and counter
-            if fruits[rightIndex] not in fruitTracker:
-                fruitTracker.add(fruits[rightIndex])
-                while rightIndex < len(fruits) and fruits[rightIndex] in fruitTracker:
-                    rightIndex += 1
-                typeCounter = len(fruitTracker)
-            
-            else:
-                rightIndex += 1
-            
-            while typeCounter >= 2:
-                # Track length after type counter == 2
-                if typeCounter == 2:
-                    currRowLength = max(currRowLength, (rightIndex) - leftIndex)
+            # track and count tree types (types are keys, and amounts are the values)
+            while typeTracker <= 2 and rightIndex <= len(fruits):
+                if typeTracker == 2:
+                    currRowLength = rightIndex - leftIndex 
                     maxRowLength = max(maxRowLength, currRowLength)
-            
-                # Track leftIndex 
-                removeFruit = fruits[leftIndex]
-                while fruits[leftIndex] == removeFruit:
+                    
+                if rightIndex < len(fruits) and fruits[rightIndex] not in fruitTree:
+                    fruitTree[fruits[rightIndex]] = 0
+                
+                if rightIndex != len(fruits):
+                    fruitTree[fruits[rightIndex]] = 1 + fruitTree.get(fruits[rightIndex], 0)
+                    typeTracker = len(fruitTree)
+                    rightIndex += 1
+                
+                else:
+                    return fruitTree[fruits[leftIndex]] if typeTracker == 1 else maxRowLength
+
+            while typeTracker > 2:
+                if fruitTree.get(fruits[leftIndex], 0) == 0:
+                    del fruitTree[fruits[leftIndex]]
                     leftIndex += 1
-                    # Increment left Index and decrement typeCounter
-                    if fruits[leftIndex] != removeFruit:
-                        fruitTracker = set()
-                        typeCounter = 0
-            
-                # Jump left index, Add current fruit to tracker and increment count
-                leftIndex = rightIndex - 1
-                fruitTracker.add(fruits[leftIndex])
-                typeCounter = len(fruitTracker)
-            
+                    typeTracker = len(fruitTree)
+                
+                else:
+                    fruitTree[fruits[leftIndex]] -= 1
+                    if fruitTree.get(fruits[leftIndex], 0) == 0:
+                        del fruitTree[fruits[leftIndex]]
+                        typeTracker = len(fruitTree)
+                    leftIndex += 1
+        
         return maxRowLength
 
-
-
-        
-                
+    
+      
 sol = Solution()
-print(sol.totalFruit([1,2,1])) # Expected: 3
-print(sol.totalFruit([0,1,2,2])) # Expected: 3
-print(sol.totalFruit([3,3,3,1,2,1,1,2,3,3,4])) # Expected: 5
+# print(sol.totalFruit([1,2,1])) # Expected: 3
+# print(sol.totalFruit([0,1,2,2])) # Expected: 3
+# print(sol.totalFruit([3,3,3,1,2,1,1,2,3,3,4])) # Expected: 5
+print(sol.totalFruit([1,1])) # Expected: 2
