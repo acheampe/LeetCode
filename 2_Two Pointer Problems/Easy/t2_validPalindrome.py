8c0f0bcefb8dd76fec2a025f21416b7b4bbad427
acheampe <acheampe@oregonstate.edu>
2024-12-16 15:31:16 -0500

    optimized v1

--- 2_Two Pointer Problems/Easy/t2_validPalindrome.py
+++ 2_Two Pointer Problems/Easy/t2_validPalindrome.py
@@ -6,38 +6,35 @@ class Solution:
         function to determine if string is a palindrome
         """
 
-        # Initiate variables
-        start = 0 
-        end = len(s) - 1
-        
-        # Establishing termination condition
-        while start < end:
+        # Initiate two pointer variables
+        start, end = 0, len(s) - 1
 
-            # if start and end chrs ==
+        while start < end:
             startLetter, endLetter = s[start].lower(), s[end].lower()
-            if startLetter == endLetter:
-                start += 1
-                end -= 1
-            
-            startLetter, endLetter = s[start].lower(), s[end].lower()
-            while start < end and ord(startLetter) < ord('a') or ord(startLetter) > ord('z'):
+
+            while start < end and startLetter.isalpha() == False and startLetter.isdigit() == False:
                 start += 1
                 startLetter = s[start].lower()
-
-
-            startLetter, endLetter = s[start].lower(), s[end].lower()
-            while end > start and ord(endLetter) < ord('a') or ord(endLetter) > ord('z'):
+            
+            while end > start and endLetter.isalpha() == False and endLetter.isdigit() == False:
                 end -= 1
                 endLetter = s[end].lower()
             
-            startLetter, endLetter = s[start].lower(), s[end].lower()
             if startLetter != endLetter:
                 return False
+            
+            else:
+                start += 1
+                end -= 1
         
         return True
 
+        
+   
+
 sol = Solution()
-# print(sol.isPalindrome("A man, a plan, a canal: Panama")) # Expected: true
-# print(sol.isPalindrome("race a car")) # Expected: false
-# print(sol.isPalindrome(" ")) # Expected: true
+print(sol.isPalindrome("A man, a plan, a canal: Panama")) # Expected: true
+print(sol.isPalindrome("race a car")) # Expected: false
+print(sol.isPalindrome(" ")) # Expected: true
 print(sol.isPalindrome(".,")) # Expected: true
+print(sol.isPalindrome("0P")) # Expected: false
