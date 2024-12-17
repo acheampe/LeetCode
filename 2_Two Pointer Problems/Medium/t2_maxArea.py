e2c4cc3f4f5a103d9fe8140c4908de4a0d3b6dee
acheampe <acheampe@oregonstate.edu>
2024-12-17 17:37:47 -0500

    first attempt

--- 2_Two Pointer Problems/Medium/t2_maxArea.py
+++ 2_Two Pointer Problems/Medium/t2_maxArea.py
@@ -1,8 +1,34 @@
 from typing import List
 
+# Time Complexity == O(n) and Space Complexity == O(1)
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        pass
+        """
+        Return the maximum amount of water a container can store.   
+        """
+
+        startPointer, endPointer = 0, len(height) - 1
+        maxArea = 0
+
+        while startPointer < endPointer:
+            
+            # Calc. current max allowable height and length
+            currMinHeight = min(height[startPointer], height[endPointer])
+            currLength = endPointer - startPointer
+
+            maxArea = max(maxArea, currMinHeight * currLength)
+
+            if height[startPointer] > height[endPointer]:
+                endPointer -=1
+            elif height[startPointer] < height[endPointer]: 
+                startPointer += 1
+            
+            else:
+                startPointer += 1
+                endPointer -=1 
+
+        
+        return maxArea
 
 sol = Solution()
 print(sol.maxArea([1,8,6,2,5,4,8,3,7])) # Expected: 49
