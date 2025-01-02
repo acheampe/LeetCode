becbe2cd60a301cd844af17e66952e53541d6d86
acheampe <acheampe@oregonstate.edu>
2025-01-01 20:04:50 -0500

    recursive Fib

--- 5_Divide and Conquer/Recurrence Relations Practice/Easy/r4_recursiveFib.py
+++ 5_Divide and Conquer/Recurrence Relations Practice/Easy/r4_recursiveFib.py
@@ -5,15 +5,15 @@ class Solution:
         """
 
         # Base/termination case:
-        if n <= 1:
-            return 1
-
-        result = self.fib(n - 1) + self.fib(n - 2) # TC = O(n) and SC = O(n) for stack use
+        if n == 0 or n == 1:
+            return 1 if n == 1 else 0
         
-        return result
+        return self.fib(n - 1) + self.fib(n - 2) # TC = O(2^n) and SC = O(n) for stack use - try with memoization when you get to DP
+    
+    # Recurrence relations: T(n) = T(n - 1) + T(n - 2)
     
 
 sol = Solution()
 print(sol.fib(2)) # Expected Output = 1
-# print(sol(3)) # Expected Output = 2
-# print(sol(4)) # Expected Output = 3
+print(sol.fib(3)) # Expected Output = 2
+print(sol.fib(4)) # Expected Output = 3
