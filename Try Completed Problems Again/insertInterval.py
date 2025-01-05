c6d17fa2c097d9fa3d5e7973ef944dec1662c181
acheampe <acheampe@oregonstate.edu>
2025-01-05 16:53:44 -0500

    insert Interval Optimized

--- Try Completed Problems Again/insertInterval.py
+++ Try Completed Problems Again/insertInterval.py
@@ -1,66 +1,43 @@
 from typing import List
 
-class Solution:
+class Solution: # Time Complexity O(n), and Space Complexity = O(n) if considering result array
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         """
         return interval in place with no overlaps"
         """
 
         # Edge case of intervals.length <= 0:
-        if not intervals:
-            return newInterval# return new intervals as is due to empty arr
+        if not intervals or not intervals[0]:
+            return newInterval # return new intervals as is due to empty arr
         
-        # Address condition for when startIndex = 0 and newInterval should be placed before it
-        if newInterval[1] < intervals[0][0]:
-            intervals.insert(0, newInterval) # results in O(n)
-            return intervals
+        # result intervals are appended here
+        resultInterval = []
 
-        # Address condition when newInterval should be place after intervals
-        if newInterval[0] > intervals[-1][1]:
-            return intervals
+        # Basic variables needed for rest of operation
+        currIndex = 0
+        arrLen = len(intervals)
+
+        # First, take case of all intervals before having the need to merge
+        while currIndex < arrLen and intervals[currIndex][1] < newInterval[0]:
+            resultInterval.append(intervals[currIndex])
+            currIndex += 1
         
-        # Needed Variables for core logic:
-        startIndex, endIndex = 0, len(intervals) - 1 # exclusive of last interval
+        # Second Merge condition
+        while currIndex < arrLen and intervals[currIndex][0] <= newInterval[1]:
+            newInterval[0] = min(newInterval[0], intervals[currIndex][0])
+            newInterval[1] = max(newInterval[1], intervals[currIndex][1])
 
-        while startIndex < endIndex:
-
-            # Condition for perfect placement without merging:
-            if newInterval[0] > intervals[startIndex][1] and newInterval[1] < intervals[startIndex][0]:
-                intervals.insert(startIndex + 1, newInterval) # Worse case: O(n)
-                return intervals
-            
-            # Merge condition:
-            while newInterval[0] <= intervals[startIndex][1]:
-                
-                mergeIndex = startIndex
-                # condition to take care of start values of intervals <= newInterval[0]
-                if newInterval[0] <= intervals[startIndex][0]:
-                    intervals[mergeIndex][0] = newInterval[0]
-                
-                    if newInterval[1] > intervals[startIndex][1] and newInterval[1] < intervals[startIndex + 1][0]:
-                        intervals[mergeIndex][1] = newInterval[1]
-                        return intervals
-                    
-                    startIndex += 1
-                    while intervals[startIndex][0] < newInterval[1] and intervals[startIndex][1] < newInterval[1] :
-                        del intervals[startIndex]
-                    
-                    if newInterval[1] < intervals[startIndex][0]:
-                        intervals[mergeIndex][1] = newInterval[1]
-                        return intervals
-                    
-                    else:
-                        intervals[mergeIndex][1] = intervals[startIndex][1]
-                        return intervals
-
-
-
-
-            
+            currIndex += 1
         
+        # Third, Append merged intervals
+        resultInterval.append(newInterval)
 
+        # Fourth, Append rest of the intervals
+        resultInterval.extend(intervals[currIndex:])
+
+        return resultInterval
 
 sol = Solution()
 print(sol.insert([[1,3],[6,9]], [2,5])) # [[1,5],[6,9]]
-# print(sol.insert([[1,2],[3,5],[6,7],[8,10],[12,16]], [4,8])) # [[1,2],[3,10],[12,16]]
+print(sol.insert([[1,2],[3,5],[6,7],[8,10],[12,16]], [4,8])) # [[1,2],[3,10],[12,16]]
 # print(sol.insert([[]], [5, 7])) # [[1,2],[3,10],[12,16]]
