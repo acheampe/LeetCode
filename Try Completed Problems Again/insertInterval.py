c9b39da10d4c860367566138840532122bda8568
acheampe <acheampe@oregonstate.edu>
2025-01-06 09:24:40 -0500

    optimized insertInterval

--- Try Completed Problems Again/insertInterval.py
+++ Try Completed Problems Again/insertInterval.py
@@ -3,39 +3,39 @@ from typing import List
 class Solution: # Time Complexity O(n), and Space Complexity = O(n) if considering result array
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         """
-        return interval in place with no overlaps"
+        Insert newInterval so that arr has no overlaps
         """
 
-        # Edge case of intervals.length <= 0:
+        # Edge case: 0 <= intervals.length <= 104
         if not intervals or not intervals[0]:
-            return newInterval # return new intervals as is due to empty arr
+            return [newInterval]
         
-        # result intervals are appended here
-        resultInterval = []
+        # Needed variables:
+        resultIntervals = [] # O(n) space to return new upated arr
+        currIndex, arrLen = 0, len(intervals)
 
-        # Basic variables needed for rest of operation
-        currIndex = 0
-        arrLen = len(intervals)
-
-        # First, take case of all intervals before having the need to merge
+        # Step 1 - Append all appropriate intervals before newInterval
         while currIndex < arrLen and intervals[currIndex][1] < newInterval[0]:
-            resultInterval.append(intervals[currIndex])
+            resultIntervals.append(intervals[currIndex])
             currIndex += 1
         
-        # Second Merge condition
-        while currIndex < arrLen and intervals[currIndex][0] <= newInterval[1]:
-            newInterval[0] = min(newInterval[0], intervals[currIndex][0])
+        # Step 2 - Merge newInterval within appropriate intervals
+        while currIndex < arrLen and newInterval[1] >= intervals[currIndex][0]:
+            # Decide the min of starti
+            newInterval[0] = min(newInterval[0], intervals[currIndex][0])        
+            # Decide the max of endi
             newInterval[1] = max(newInterval[1], intervals[currIndex][1])
 
             currIndex += 1
+
+        # Append the new Intervals
+        resultIntervals.append(newInterval)  
+
+        # Append rest of intervals
+        resultIntervals.extend(intervals[currIndex : ])
+
+        return resultIntervals
         
-        # Third, Append merged intervals
-        resultInterval.append(newInterval)
-
-        # Fourth, Append rest of the intervals
-        resultInterval.extend(intervals[currIndex:])
-
-        return resultInterval
 
 sol = Solution()
 print(sol.insert([[1,3],[6,9]], [2,5])) # [[1,5],[6,9]]
