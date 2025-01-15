39cf459b78154d5488fe5a4e3236bb1d1997058e
acheampe <acheampe@oregonstate.edu>
2025-01-15 12:47:44 -0500

    Brufe Force Approach to PathSum III

--- 9_Depth First Search/Medium/d3_recurPathSumIII.py
+++ 9_Depth First Search/Medium/d3_recurPathSumIII.py
@@ -1,4 +1,5 @@
 from typing import Optional, List
+from collections import deque
 
 # Definition for a binary tree node.
 class TreeNode:
@@ -7,10 +8,85 @@ class TreeNode:
         self.left = left
         self.right = right
 
+# class Solution:
+#     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
+#         """
+#         return number of paths that eventually sums up to targetSum along 
+#         its path
+#         """
+
+#         if not root:
+#             return 0 # cant sum up to targetSum if there is no path
+        
+#         self.pathCount = 0
+#         path = deque()
+#         pathSum = 0
+
+#         self.pathCountHelper(root, targetSum, path, pathSum)
+
+#         return self.pathCount
+
+#     def pathCountHelper(self, node, targetSum, path, pathSum):
+#         """
+#         count valid paths that can sum up to target
+#         """
+
+
+#         if not node:
+#             return
+        
+#         path.append(node.val)
+#         pathSum += node.val
+
+#         if pathSum == targetSum:
+#             self.pathCount += 1
+#             pathSum -= path.popleft()
+
+#         elif not node.left and not node.right and pathSum > targetSum: # reduce pathSum value
+#             while path and pathSum > targetSum:
+#                 pathSum -= path.popleft()
+
+#                 if pathSum == targetSum:
+#                     self.pathCount += 1
+        
+#         elif pathSum > targetSum:
+#             pathSum -= path.popleft()
+        
+#         self.pathCountHelper(node.left, targetSum, path + deque([]), pathSum)
+#         self.pathCountHelper(node.right, targetSum, path + deque([]), pathSum)
+
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
-        pass
+        """
+        Return the number of paths where the sum of the values equals targetSum.
+        """
 
+        ### BRUTE FORCE APPROACH...NEED TO LEARN PREFIX SUM TECHNIQUE...O(n^2) complexity ###
+
+        if not root:
+            return 0
+
+        # Count paths starting from the current node
+        def countPathsFromNode(node, targetSum):
+            if not node:
+                return 0
+
+            count = 0
+            if node.val == targetSum:
+                count += 1
+
+            # Recurse on left and right children
+            count += countPathsFromNode(node.left, targetSum - node.val)
+            count += countPathsFromNode(node.right, targetSum - node.val)
+
+            return count
+
+        # Total paths = paths starting at root + paths in left subtree + paths in right subtree
+        return (countPathsFromNode(root, targetSum) +
+                self.pathSum(root.left, targetSum) +
+                self.pathSum(root.right, targetSum))
+
+        
 # Helper function to build a binary tree from a list
 def build_tree(values: List[Optional[int]]) -> Optional[TreeNode]:
     if not values:
