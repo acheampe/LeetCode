6c9c4ec3c8636cb86a8a74171e74dec1464803b9
acheampe <acheampe@oregonstate.edu>
2025-01-16 17:26:44 -0500

    recur Solution

--- 9_Depth First Search/Medium/d3_recurPathSumIII.py
+++ 9_Depth First Search/Medium/d3_recurPathSumIII.py
@@ -8,54 +8,8 @@ class TreeNode:
         self.left = left
         self.right = right
 
-# class Solution:
-#     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
-#         """
-#         return number of paths that eventually sums up to targetSum along 
-#         its path
-#         """
+class Solution: ### RECURSIVE APPROACH ####
 
-#         if not root:
-#             return 0 # cant sum up to targetSum if there is no path
-        
-#         self.pathCount = 0
-#         path = deque()
-#         pathSum = 0
-
-#         self.pathCountHelper(root, targetSum, path, pathSum)
-
-#         return self.pathCount
-
-#     def pathCountHelper(self, node, targetSum, path, pathSum):
-#         """
-#         count valid paths that can sum up to target
-#         """
-
-
-#         if not node:
-#             return
-        
-#         path.append(node.val)
-#         pathSum += node.val
-
-#         if pathSum == targetSum:
-#             self.pathCount += 1
-#             pathSum -= path.popleft()
-
-#         elif not node.left and not node.right and pathSum > targetSum: # reduce pathSum value
-#             while path and pathSum > targetSum:
-#                 pathSum -= path.popleft()
-
-#                 if pathSum == targetSum:
-#                     self.pathCount += 1
-        
-#         elif pathSum > targetSum:
-#             pathSum -= path.popleft()
-        
-#         self.pathCountHelper(node.left, targetSum, path + deque([]), pathSum)
-#         self.pathCountHelper(node.right, targetSum, path + deque([]), pathSum)
-
-class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
         """
         Return the number of paths where the sum of the values equals targetSum.
