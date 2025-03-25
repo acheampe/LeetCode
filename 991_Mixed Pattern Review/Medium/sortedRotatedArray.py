a73fdaac6394ad5b198ee6252ffcce856060610d
acheampe <acheampe@oregonstate.edu>
2025-03-25 15:07:39 -0400

    sortedRotatedArray

--- 991_Mixed Pattern Review/Medium/sortedRotatedArray.py
+++ 991_Mixed Pattern Review/Medium/sortedRotatedArray.py
@@ -1,10 +1,43 @@
 import unittest
 
-# Assume this is the solution stub you provided
 class Solution:
     def search(self, nums: list[int], target: int) -> int:
-        pass
-
+        """
+        return index of target if target in nums
+        
+        TC: O(log n)
+        SC: O(1)
+        
+        QUICK Summary: Main trick is to figure out which side is ordered, then 
+        based of that decide which side to search for our target value. 
+        """
+        
+        leftIndex, rightIndex = 0, len(nums) - 1
+        
+        while leftIndex < rightIndex:
+            
+            midIndex = leftIndex + (rightIndex - leftIndex) // 2
+            
+            if nums[midIndex] == target:
+                return midIndex
+            
+            elif nums[leftIndex] <= nums[midIndex]: # means left side is the ordered side
+                
+                if (nums[leftIndex] <= target < nums[midIndex]):
+                    rightIndex = midIndex - 1
+                
+                else:
+                    leftIndex = midIndex + 1
+            
+            else:
+                if (nums[rightIndex] >= target > nums[midIndex]):
+                    leftIndex = midIndex + 1
+                
+                else:
+                    rightIndex = midIndex - 1
+        
+        return leftIndex if nums[leftIndex] == target else -1
+    
 class TestSearchInRotatedSortedArray(unittest.TestCase):
     def setUp(self):
         self.sol = Solution()
@@ -34,6 +67,9 @@ class TestSearchInRotatedSortedArray(unittest.TestCase):
     def test_large_array_target_present(self):
         nums = list(range(1000, 10000)) + list(range(1, 999))
         self.assertEqual(self.sol.search(nums, 1234), nums.index(1234))
+
+    def test_example_0(self):
+        self.assertEqual(self.sol.search([1,3], 3), 1)
 
 if __name__ == "__main__":
     unittest.main()
