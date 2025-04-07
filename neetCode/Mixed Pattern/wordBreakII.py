68e590d54ba03dd5702995ba75037e7930c0a302
acheampe <acheampe@oregonstate.edu>
2025-04-06 20:43:40 -0400

    wordBreak II

--- neetCode/Mixed Pattern/wordBreakII.py
+++ neetCode/Mixed Pattern/wordBreakII.py
@@ -68,16 +68,16 @@ class TestSolution(unittest.TestCase):
         sol = Solution()
         
         # Basic test cases
-        self.assertCountEqual(sol.wordBreak("catsanddog", ["cat","cats","and","sand","dog"]), 
-                              ["cats and dog", "cat sand dog"])
-        self.assertCountEqual(sol.wordBreak("pineapplepenapple", ["apple","pen","applepen","pine","pineapple"]), 
-                              ["pine apple pen apple","pineapple pen apple","pine applepen apple"])
-        self.assertCountEqual(sol.wordBreak("catsandog", ["cats","dog","sand","and","cat"]), 
-                              [])
+        # self.assertCountEqual(sol.wordBreak("catsanddog", ["cat","cats","and","sand","dog"]), 
+        #                       ["cats and dog", "cat sand dog"])
+        # self.assertCountEqual(sol.wordBreak("pineapplepenapple", ["apple","pen","applepen","pine","pineapple"]), 
+        #                       ["pine apple pen apple","pineapple pen apple","pine applepen apple"])
+        # self.assertCountEqual(sol.wordBreak("catsandog", ["cats","dog","sand","and","cat"]), 
+        #                       [])
 
-        # Edge cases
-        self.assertCountEqual(sol.wordBreak("", ["cat","dog"]), [])  # Empty string case
-        self.assertCountEqual(sol.wordBreak("a", ["a"]), ["a"])  # Single character match
+        # # # Edge cases
+        # self.assertCountEqual(sol.wordBreak("", ["cat","dog"]), [])  # Empty string case
+        # self.assertCountEqual(sol.wordBreak("a", ["a"]), ["a"])  # Single character match
         self.assertCountEqual(sol.wordBreak("abcd", ["a","abc","b","cd"]), ["a b cd", "abc d"])  # Multiple valid sentences
 
         # # Large input case
