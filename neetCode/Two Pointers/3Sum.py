6910deafcb2d8dc34b40370cdade91fe103d8088
acheampe <acheampe@oregonstate.edu>
2025-04-12 11:17:28 -0400

    3Sum

--- neetCode/Two Pointers/3Sum.py
+++ neetCode/Two Pointers/3Sum.py
@@ -1,55 +1,89 @@
+# class Solution:
+#     def threeSum(self, nums: list[int]) -> list[list[int]]:
+#         """return all tripets in an array that sums to 0"""
+        
+#         def discussQuestionApproach():
+#             """
+#             Given the constraints I do not have to worry about input arr len
+#             less than 3 nor should I worry about at empty input
+            
+#             if none is found, I will return an empty array
+            
+#             I can use a 3 pointer system to find the answer in O(n^2) TC and
+#             O(1) space complexity, not calculating for output array
+#             """
+#             pass
+#         # Given that input arr can be sorted:
+#         nums.sort()
+#         result = [] # returns lists that sums to 0
+        
+#         for i in range(len(nums)):
+            
+#             # To avoid duplicates
+#             if i != 0 and nums[i] == nums[i - 1]:
+#                 continue #skips current iter in order to avoid duplicates
+            
+#             j, k = i + 1, len(nums) - 1
+            
+#             while j < k:
+#                 total = nums[i] + nums[j] + nums[k]
+#                 if total == 0:
+#                     result.append([nums[i], nums[j], nums[k]])
+                    
+#                     # Move j and skip duplicates
+#                     j += 1
+#                     while j < k and nums[j] == nums[j - 1]:
+#                         j += 1
+
+#                     # Move k and skip duplicates
+#                     k -= 1
+#                     while j < k and nums[k] == nums[k + 1]:
+#                         k -= 1
+
+#                 elif total < 0:
+#                     j += 1
+#                 else:
+#                     k -= 1
+
+
+                               
+#         return result      
+        
 class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
-        """return all tripets in an array that sums to 0"""
-        
-        def discussQuestionApproach():
-            """
-            Given the constraints I do not have to worry about input arr len
-            less than 3 nor should I worry about at empty input
-            
-            if none is found, I will return an empty array
-            
-            I can use a 3 pointer system to find the answer in O(n^2) TC and
-            O(1) space complexity, not calculating for output array
-            """
-            pass
-        # Given that input arr can be sorted:
+        """return all triplets in input nums that sums to 0"""
+
+        result = []
         nums.sort()
-        result = [] # returns lists that sums to 0
-        
+
         for i in range(len(nums)):
-            
-            # To avoid duplicates
+
             if i != 0 and nums[i] == nums[i - 1]:
-                continue #skips current iter in order to avoid duplicates
+                continue # avoid duplicates
             
             j, k = i + 1, len(nums) - 1
-            
+
             while j < k:
                 total = nums[i] + nums[j] + nums[k]
                 if total == 0:
                     result.append([nums[i], nums[j], nums[k]])
-                    
-                    # Move j and skip duplicates
+
+                    # To avoid duplicate results
                     j += 1
                     while j < k and nums[j] == nums[j - 1]:
                         j += 1
-
-                    # Move k and skip duplicates
+                    
                     k -= 1
                     while j < k and nums[k] == nums[k + 1]:
                         k -= 1
-
-                elif total < 0:
-                    j += 1
-                else:
+                
+                elif total > 0:
                     k -= 1
-
-
-                               
-        return result      
-        
-    
+                
+                else:
+                    j += 1
+            
+        return result
     
 sol = Solution()
 print(sol.threeSum([-1,0,1,2,-1,-4])) # Expected: [[-1,-1,2],[-1,0,1]]
