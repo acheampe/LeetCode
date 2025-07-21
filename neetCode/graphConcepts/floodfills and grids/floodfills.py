70be90ee84507550a4b8819eb055a015f37c0a1b
acheampe <acheampe@oregonstate.edu>
2025-07-21 19:02:18 -0400

    floodfill

--- neetCode/graphConcepts/floodfills and grids/floodfills.py
+++ neetCode/graphConcepts/floodfills and grids/floodfills.py
@@ -2,42 +2,37 @@ import unittest
 
 class Solution:
     def floodFill(self, image: list[list[int]], sr: int, sc: int, color: int) -> list[list[int]]:
-        
-        currentColor = image[sr][sc]
-        
-        if currentColor == color:
-            return image
-        
+
+        currColor = image[sr][sc]
         seen = set()
-        
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)] # right, left, down, up respectively for adjacent checks. If diagonals were needed, 4 more coordinates will be integrated
+
+        #check if we need to do floodfill 
+        if currColor == color:
+            return image # no further changes needed
+
         def dfs(r, c):
-            # base case: out of bounds
-            if r < 0 or r >= len(image) or c < 0 or c >= len(image[0]):
-                return
             
-            # already visited
+            # create base conditions
+            if r < 0 or r >= len(image) or c < 0 or c >= len(image[0]):
+                return # invalid coordinates
+
             if (r, c) in seen:
                 return
             
-            # not the same color → don't flood
-            if image[r][c] != currentColor:
-                return
-
-            # mark visited
-            seen.add((r, c))
-
-            # flood fill current
+            if image[r][c] != currColor:
+                return # no need to proceed further, only wanna change valid fields
+            
+            seen.add((r,c))
             image[r][c] = color
 
-            # explore neighbors
-            dfs(r + 1, c)
-            dfs(r - 1, c)
-            dfs(r, c + 1)
-            dfs(r, c - 1)
-                
-        dfs(sr, sc)
+            for dr, dc in directions:
+                dfs(r + dr, c + dc) # SC = O(m * n) for recursive stack and TC = O(m *n) for visiting all cells.
         
+        dfs(sr, sc)
         return image
+        
+       
 
 
 class TestFloodFill(unittest.TestCase):
