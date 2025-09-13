f94b8a257036a584a6873efa23975e2606c4215e
acheampe <acheampe@oregonstate.edu>
2025-09-13 12:01:19 -0400

    floodfill.py and tdd_floodfill.py set up

---
+++ realWorldProjectApplication/Satellite Image Analyzer/TDD/tdd_floodfill.py
@@ -0,0 +1,13 @@
+import unittest
+
+from gridtools.floodfill import (
+    pass
+)
+
+class TestFloodfill(unittest.TestCase):
+    
+    def setUp(self) -> None:
+        
+        grid = [
+            ...
+        ]
---
+++ realWorldProjectApplication/Satellite Image Analyzer/src/gridtools/floodfill.py
@@ -0,0 +1,11 @@
+from gridtools.connectivity import (
+    neighbors_4, 
+    neighbors_8,
+)
+from gridtools.predicates import (
+    is_within_bound,
+    is_water,
+    is_land,
+    is_edge_coord,
+    is_interior_coord,
+)
