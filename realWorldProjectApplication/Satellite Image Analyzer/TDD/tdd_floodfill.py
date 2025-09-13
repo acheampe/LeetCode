68100864ee2ad734e092aa0f18d0d4f0eba22b82
acheampe <acheampe@oregonstate.edu>
2025-09-13 13:39:32 -0400

    test_out_of_bound_floodfill - failed

--- realWorldProjectApplication/Satellite Image Analyzer/TDD/tdd_floodfill.py
+++ realWorldProjectApplication/Satellite Image Analyzer/TDD/tdd_floodfill.py
@@ -10,9 +10,9 @@ class TestFloodfill(unittest.TestCase):
     def setUp(self) -> None:
         
         self.grid = [
-            [1, 1, 1, 1],
-            [1, 0, 1, 0],
-            [0, 1, 1, 1]
+            [1, 1, 0, 0],
+            [1, 0, 0, 1],
+            [0, 1, 0, 0]
         ]
         
         # Legend: 0 = land, 1 = water
@@ -26,10 +26,10 @@ class TestFloodfill(unittest.TestCase):
         
     def test_out_of_bound_floodfill(self):
         expected: list[tuple[int, int]] = []
-        result = floodfill(3, 3, self.grid)
+        result = floodfill(4, 4, self.grid)
         self.assertEqual(result, 
                             expected, 
-                            msg=f"floodfill for (3, 3) returned {result}, expected {expected}")
+                            msg=f"floodfill for (4, 4) returned {result}, expected {expected}")
             
     def test_isolated_water_cell(self):
         expected: list[tuple[int, int]] = [(1, 3)]
--- realWorldProjectApplication/Satellite Image Analyzer/src/gridtools/floodfill.py
+++ realWorldProjectApplication/Satellite Image Analyzer/src/gridtools/floodfill.py
@@ -10,11 +10,26 @@ from gridtools.predicates import (
     is_interior_coord,
 )
 
-def fill(i: int, j: int, grid: list[list[int]]):
-    return []
+def fill(i: int, j: int, grid: list[list[int]]) -> list[tuple[int, int]]:
+    """returns all neighbors per directions"""
+    return neighbors_4(i, j, grid)
 
 def floodfill(k: int, l: int, curr_grid: list[list[int]]):
-    return fill(k, l, curr_grid)
 
-def landfill(k: int, l: int, curr_grid: list[list[int]]):
-    return fill(k, l, curr_grid)
+    valid_nei: list[tuple[int, int]] = []
+    
+    # check curr coords if land (0) or water (1)
+    if curr_grid[k][l] == 1:
+        valid_nei.append((k, l))
+        curr_nei = fill(k, l, curr_grid)
+    
+        for nei in curr_nei:
+            r, c = nei
+            if curr_grid[r][c] == 1:
+                valid_nei.append((r, c))
+    
+    return valid_nei
+                
+
+# def landfill(k: int, l: int, curr_grid: list[list[int]]):
+#     return fill(k, l, curr_grid)
