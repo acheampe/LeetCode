9a0e549a683749a5be9f45b945bcbf3234b8479a
acheampe <acheampe@oregonstate.edu>
2025-09-15 12:24:42 -0400

    test_grid_with_all_water_cells_nei_4 - Passed

--- realWorldProjectApplication/Satellite Image Analyzer/src/gridtools/floodfill.py
+++ realWorldProjectApplication/Satellite Image Analyzer/src/gridtools/floodfill.py
@@ -1,4 +1,5 @@
-from collections.abc import Callable 
+from collections import deque
+from collections.abc import Callable
 
 from gridtools.connectivity import (
     neighbors_4, 
@@ -16,19 +17,33 @@ from gridtools.predicates import (
 NeighborFn = Callable[[int, int, list[list[int]]], list[tuple[int, int]]]
 
 def floodfill(k: int, l: int, curr_grid: list[list[int]], neighbor_fn: NeighborFn):
-
+    """returns all connected lake cells in grid"""
+    
+    # defensive assertions
+    assert len(curr_grid) > 0, f"No rows found in {curr_grid}"
+    assert len(curr_grid[0]) > 0, f"No column found in {curr_grid}"
+    
+    explore_nei = deque()
     valid_nei: list[tuple[int, int]] = []
+    
+    mark_visited: set = set()
     
     # check curr coords if land (0) or water (1)
     if is_within_bound(k, l, curr_grid) and is_water(k, l, curr_grid):
-        valid_nei.append((k, l))
-        curr_nei: list[tuple[int, int]] = neighbor_fn(k, l, curr_grid)
-    
-        for nei in curr_nei:
-            r, c = nei
-            if is_water(r, c, curr_grid):
-                valid_nei.append((r, c))
-    
+        explore_nei.append((k, l))
+
+    while explore_nei:
+        
+        qr, qc = explore_nei.popleft()
+        
+        if is_water(qr, qc, curr_grid) and (qr, qc) not in mark_visited:
+            valid_nei.append((qr, qc))
+            mark_visited.add((qr, qc))
+            next_neigbors = neighbor_fn(qr, qc, curr_grid)
+            
+            for coord in next_neigbors:
+                explore_nei.append(coord)
+            
     return valid_nei
                 
 
