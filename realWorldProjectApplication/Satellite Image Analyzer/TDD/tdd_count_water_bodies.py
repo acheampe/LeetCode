344171df39d864edfd704de42bd0a6843c8f5845
acheampe <acheampe@oregonstate.edu>
2025-09-16 09:29:49 -0400

    removed unused imports

---
+++ realWorldProjectApplication/Satellite Image Analyzer/TDD/tdd_count_water_bodies.py
@@ -0,0 +1,38 @@
+import unittest
+
+from gridtools.features.count_water_bodies import count_water_bodies
+
+from gridtools.connectivity import (
+    neighbors_4,
+    neighbors_8,
+)
+
+class TestCountWaterBodies(unittest.TestCase):
+    
+    def setUp(self) -> None:
+        
+        # Legends: 1 = water, 0 = land
+        self.uniformed_grid = [
+            [1, 1, 1],
+            [1, 1, 1],
+            [1, 1, 1],
+        ]
+        
+        self.complex_grid = [
+            [1, 1, 0, 1],
+            [0, 0, 1, 0],
+            [0, 1, 1, 0],
+            [1, 0, 1, 0]
+        ]
+        
+        self.empty_grid = [
+            []
+        ]
+        
+        def test_empty_grid_returns_zero_with_neighbors_4(self):
+            expected: int = 0
+            result: int = count_water_bodies(self.empty_grid, neighbors_4)
+            self.assertEqual(result, expected, 
+                                msg=f"Amount of bodies of water returned {result}, expected {expected}"
+                                )
+            
--- realWorldProjectApplication/Satellite Image Analyzer/src/gridtools/floodfill.py
+++ realWorldProjectApplication/Satellite Image Analyzer/src/gridtools/floodfill.py
@@ -1,16 +1,9 @@
 from collections import deque
 from collections.abc import Callable
 
-from gridtools.connectivity import (
-    neighbors_4, 
-    neighbors_8,
-)
 from gridtools.predicates import (
     is_within_bound,
     is_water,
-    is_land,
-    is_edge_coord,
-    is_interior_coord,
 )
 
 # calling a function with args [int, int, list[list[int]]] that returns a list[tuple[int, int]]
