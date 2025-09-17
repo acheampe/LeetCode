597e6d0df8b260a0d86ba9ab07394194e600ef25
acheampe <acheampe@oregonstate.edu>
2025-09-17 11:38:01 -0400

    enclosed_count_water_bodies

---
+++ realWorldProjectApplication/Satellite Image Analyzer/TDD/tdd_count_enclosed_lakes.py
@@ -0,0 +1,50 @@
+import unittest
+
+class TestCountEnclosedLakes(unittest.TestCase):
+    
+    def setUp(self) -> None:
+        
+        # Legends: 1 = water, 0 = land
+        self.empty_grid = []
+        
+        self.empty_nested_grid = [
+            []
+            ]
+        
+        self.cornered_water_grid = [
+                [1,0,1],
+                [0,0,0],
+                [1,0,1],
+        ]
+        self.uniformed_grid = [
+            [1, 1, 1],
+            [1, 1, 1],
+            [1, 1, 1],
+        ]
+
+        self.complex_grid_no_enclosed = [
+            [1, 1, 0, 1],
+            [0, 0, 1, 0],
+            [0, 1, 1, 0],
+            [1, 0, 1, 0],
+        ]
+
+        # no enclosed for neighbor_8, 1 enclosed for neighbor_4
+        self.complex_gridv2 = [
+            [1, 1, 0, 1],
+            [0, 0, 1, 0],
+            [0, 1, 1, 0],
+            [1, 0, 1, 0],
+            [0, 0, 0, 0],
+            [1, 1, 0, 0],
+        ]
+
+        # 3 enclosed for neighbor_4, 2 enclosed for neighbor_8
+        self.complex_gridv3 = [
+            [1, 0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 1, 0],
+            [0, 1, 1, 0, 0, 1, 1, 0],
+            [1, 0, 1, 0, 1, 0, 1, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [1, 1, 0, 0, 1, 1, 0, 0],
+        ]
