1b2830671e79c482e98c9586e76c55ae345a53ba
acheampe <acheampe@oregonstate.edu>
2025-09-18 09:57:30 -0400

    test_one_land_cell_return_zero - Passed

--- realWorldProjectApplication/Satellite Image Analyzer/TDD/tdd_max_water_area.py
+++ realWorldProjectApplication/Satellite Image Analyzer/TDD/tdd_max_water_area.py
@@ -9,6 +9,7 @@ from gridtools.connectivity import (
 
 class TestMaxWaterBodies(unittest.TestCase):
     
+    # Legends: 1 = water, 0 = land
     def test_empty_grid_return_zero(self):
         grid = []
 
@@ -29,11 +30,28 @@ class TestMaxWaterBodies(unittest.TestCase):
 
         self.assertEqual(max_water_area(grid, neighbors_4),
                             0,
-                            msg = f"expected 0 since grid is empty"
+                            msg = f"expected 0 since nested grid is empty"
                             )
 
         self.assertEqual(max_water_area(grid, neighbors_8),
                             0,
-                            msg = f"expected 0 since grid is empty"
-                            )         
+                            msg = f"expected 0 since nested grid is empty"
+                            )
+    
+    def test_one_land_cell_return_zero(self):
+        grid = [
+            [0]
+        ]
+
+        self.assertEqual(max_water_area(grid, neighbors_4),
+                            0,
+                            msg = f"expected 0 since grid only contains land"
+                            )
+
+        self.assertEqual(max_water_area(grid, neighbors_8),
+                            0,
+                            msg = f"expected 0 since grid only contains land"
+                            ) 
+        
+                  
               
