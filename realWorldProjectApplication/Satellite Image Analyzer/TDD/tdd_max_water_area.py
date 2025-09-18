b013c936a83a8c4494674180f69512d38005640b
acheampe <acheampe@oregonstate.edu>
2025-09-18 09:54:14 -0400

    test_empty_nested_grid_return_zero - passed

--- realWorldProjectApplication/Satellite Image Analyzer/TDD/tdd_max_water_area.py
+++ realWorldProjectApplication/Satellite Image Analyzer/TDD/tdd_max_water_area.py
@@ -20,4 +20,20 @@ class TestMaxWaterBodies(unittest.TestCase):
         self.assertEqual(max_water_area(grid, neighbors_8),
                             0,
                             msg = f"expected 0 since grid is empty"
-                            )       
+                            ) 
+        
+    def test_empty_nested_grid_return_zero(self):
+        grid = [
+            []
+        ]
+
+        self.assertEqual(max_water_area(grid, neighbors_4),
+                            0,
+                            msg = f"expected 0 since grid is empty"
+                            )
+
+        self.assertEqual(max_water_area(grid, neighbors_8),
+                            0,
+                            msg = f"expected 0 since grid is empty"
+                            )         
+              
