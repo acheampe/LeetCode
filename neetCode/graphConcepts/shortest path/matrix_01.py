a468366109d8b1d27b0066397a5373d1d2e176e0
acheampe <acheampe@oregonstate.edu>
2025-09-28 17:22:02 -0400

    matrix_01 - stuck

--- neetCode/graphConcepts/shortest path/matrix_01.py
+++ neetCode/graphConcepts/shortest path/matrix_01.py
@@ -8,25 +8,31 @@ class Solution:
         
         def findMinDistance(r, c, currMinDist) -> int:
             
-            if (0 > r or r >= m) or (0 > c or c >= n) or mat[r][c] == 0 or (r, c) in trackVisited:
+            if (0 > r or r >= m) or (0 > c or c >= n) or mat[r][c] == 0:
                 return 0
             
-            trackVisited.add((r, c))
-            currMinDist += (findMinDistance(r, c + 1, currMinDist + 1) + 
-                            findMinDistance(r, c - 1, currMinDist + 1) + 
-                            findMinDistance(r - 1, c, currMinDist + 1) + 
-                            findMinDistance(r + 1, c, currMinDist + 1))
-    
-            mat[r][c] = currMinDist
+            if (r, c) in trackVisited:
+                return mat[r][c]
             
-            return currMinDist + 1
-                
+            trackVisited.add((r, c))
+            currMinDist = 1
+            currMinDist += (findMinDistance(r, c + 1, currMinDist) +
+                            findMinDistance(r, c - 1, currMinDist) +
+                            findMinDistance(r - 1, c, currMinDist) +
+                            findMinDistance(r + 1, c, currMinDist))
+    
+            mat[r][c] = currMinDist 
+            
+            return mat[r][c]
         
         for i in range(m):
             for j in range(n):
-                if (i, j) not in trackVisited:
+                if (i, j) not in trackVisited and mat[i][j] != 0:
                     minDistance = 0
                     findMinDistance(i, j, minDistance)
+                
+                else:
+                    trackVisited.add((i, j))
         
         return mat
         
@@ -37,51 +43,62 @@ class TestUpdateMatrix(unittest.TestCase):
     def setUp(self):
         self.sol = Solution()
 
-    def test_case_1(self):
+    # def test_case_1(self):
+    #     mat = [
+    #         [0, 0, 0],
+    #         [0, 1, 0], 
+    #         [0, 0, 0] 
+    #     ]
+    #     expected = [
+    #         [0, 0, 0],
+    #         [0, 1, 0],
+    #         [0, 0, 0]
+    #     ]
+    #     self.assertEqual(self.sol.updateMatrix(mat), expected)
+
+    # def test_case_2(self):
+    #     mat = [
+    #         [0, 0, 0],
+    #         [0, 1, 0],
+    #         [1, 1, 1]
+    #     ]
+    #     expected = [
+    #         [0, 0, 0],
+    #         [0, 1, 0],
+    #         [1, 2, 1]
+    #     ]
+    #     self.assertEqual(self.sol.updateMatrix(mat), expected)
+
+    # def test_case_3(self):
+    #     mat = [
+    #         [1, 1, 1],
+    #         [1, 1, 1],
+    #         [0, 1, 1]
+    #     ]
+    #     expected = [
+    #         [2, 3, 4],
+    #         [1, 2, 3],
+    #         [0, 1, 2]
+    #     ]
+    #     self.assertEqual(self.sol.updateMatrix(mat), expected)
+
+    def test_case_4(self):
         mat = [
-            [0, 0, 0],
-            [0, 1, 0],
-            [0, 0, 0]
+            [1, 1, 0],
         ]
         expected = [
-            [0, 0, 0],
-            [0, 1, 0],
-            [0, 0, 0]
+            [2, 1, 0],
         ]
         self.assertEqual(self.sol.updateMatrix(mat), expected)
-
-#     def test_case_2(self):
-#         mat = [
-#             [0, 0, 0],
-#             [0, 1, 0],
-#             [1, 1, 1]
-#         ]
-#         expected = [
-#             [0, 0, 0],
-#             [0, 1, 0],
-#             [1, 2, 1]
-#         ]
-#         self.assertEqual(self.sol.updateMatrix(mat), expected)
-
-#     def test_case_3(self):
-#         mat = [
-#             [1, 1, 1],
-#             [1, 1, 1],
-#             [0, 1, 1]
-#         ]
-#         expected = [
-#             [2, 3, 4],
-#             [1, 2, 3],
-#             [0, 1, 2]
-#         ]
-#         self.assertEqual(self.sol.updateMatrix(mat), expected)
-
-    # def test_case_4(self):
+    
+    # def test_case_5(self):
     #     mat = [
     #         [1, 1, 0],
+    #         [1, 1, 1]
     #     ]
     #     expected = [
     #         [2, 1, 0],
+    #         [3, 2, 1]
     #     ]
     #     self.assertEqual(self.sol.updateMatrix(mat), expected)
         
