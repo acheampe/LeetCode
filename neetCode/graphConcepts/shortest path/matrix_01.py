4c7531be1d8b513d3d9e4825559bc733afe0d8f1
acheampe <acheampe@oregonstate.edu>
2025-10-03 11:04:37 -0400

    failed DFS approach but understand the logic

--- neetCode/graphConcepts/shortest path/matrix_01.py
+++ neetCode/graphConcepts/shortest path/matrix_01.py
@@ -1,41 +1,60 @@
-# Leetcode 542i
+# Leetcode 542
 import unittest
+
+
 class Solution:
     def updateMatrix(self, mat: list[list[int]]) -> list[list[int]]:
 
         m, n = len(mat), len(mat[0]) # if this was a production code, I would be more defensive here
-        trackVisited: set = set()
         
-        def findMinDistance(r, c, currMinDist) -> int:
+        memo: dict[tuple[int, int], float] = {}
+        curr_path: set[tuple[int, int]] = set()
+        
+        def findMinDistance(r, c) -> float:
             
-            if (0 > r or r >= m) or (0 > c or c >= n) or mat[r][c] == 0:
+            # if OOB
+            if (0 > r or r >= m) or (0 > c or c >= n) or (r, c) in curr_path:
+                return float('inf')
+            
+            # if cell is zero
+            if mat[r][c] == 0:
                 return 0
             
-            if (r, c) in trackVisited:
-                return mat[r][c]
+            # utilize memo to reduce recursive dependence
+            if (r, c) in memo:
+                return memo[(r, c)]
             
-            trackVisited.add((r, c))
-            currMinDist = 1
-            currMinDist += (findMinDistance(r, c + 1, currMinDist) +
-                            findMinDistance(r, c - 1, currMinDist) +
-                            findMinDistance(r - 1, c, currMinDist) +
-                            findMinDistance(r + 1, c, currMinDist))
-    
-            mat[r][c] = currMinDist 
+            curr_path.add((r, c))
+            result = [
+                findMinDistance(r, c + 1),
+                            findMinDistance(r, c - 1),
+                            findMinDistance(r - 1, c),
+                            findMinDistance(r + 1, c,)
+            ]
             
-            return mat[r][c]
+            curr_path.remove((r, c))
+            
+            min_dist =  min(result)
+            
+            if min_dist == float('inf'):
+                return float('inf')
+
+            dist = min_dist + 1.0
+            memo[(r, c)] = dist
+            
+            return dist
         
         for i in range(m):
             for j in range(n):
-                if (i, j) not in trackVisited and mat[i][j] != 0:
-                    minDistance = 0
-                    findMinDistance(i, j, minDistance)
-                
-                else:
-                    trackVisited.add((i, j))
+                if mat[i][j] != 0:
+                    findMinDistance(i, j)
+
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j] != 0:
+                    mat[i][j] = int(memo[(i, j)])
         
         return mat
-        
 
 
 class TestUpdateMatrix(unittest.TestCase):
@@ -43,44 +62,44 @@ class TestUpdateMatrix(unittest.TestCase):
     def setUp(self):
         self.sol = Solution()
 
-    # def test_case_1(self):
-    #     mat = [
-    #         [0, 0, 0],
-    #         [0, 1, 0], 
-    #         [0, 0, 0] 
-    #     ]
-    #     expected = [
-    #         [0, 0, 0],
-    #         [0, 1, 0],
-    #         [0, 0, 0]
-    #     ]
-    #     self.assertEqual(self.sol.updateMatrix(mat), expected)
+    def test_case_1(self):
+        mat = [
+            [0, 0, 0],
+            [0, 1, 0], 
+            [0, 0, 0] 
+        ]
+        expected = [
+            [0, 0, 0],
+            [0, 1, 0],
+            [0, 0, 0]
+        ]
+        self.assertEqual(self.sol.updateMatrix(mat), expected)
 
-    # def test_case_2(self):
-    #     mat = [
-    #         [0, 0, 0],
-    #         [0, 1, 0],
-    #         [1, 1, 1]
-    #     ]
-    #     expected = [
-    #         [0, 0, 0],
-    #         [0, 1, 0],
-    #         [1, 2, 1]
-    #     ]
-    #     self.assertEqual(self.sol.updateMatrix(mat), expected)
+    def test_case_2(self):
+        mat = [
+            [0, 0, 0],
+            [0, 1, 0],
+            [1, 1, 1]
+        ]
+        expected = [
+            [0, 0, 0],
+            [0, 1, 0],
+            [1, 2, 1]
+        ]
+        self.assertEqual(self.sol.updateMatrix(mat), expected)
 
-    # def test_case_3(self):
-    #     mat = [
-    #         [1, 1, 1],
-    #         [1, 1, 1],
-    #         [0, 1, 1]
-    #     ]
-    #     expected = [
-    #         [2, 3, 4],
-    #         [1, 2, 3],
-    #         [0, 1, 2]
-    #     ]
-    #     self.assertEqual(self.sol.updateMatrix(mat), expected)
+    def test_case_3(self):
+        mat = [
+            [1, 1, 1],
+            [1, 1, 1],
+            [0, 1, 1]
+        ]
+        expected = [
+            [2, 3, 4],
+            [1, 2, 3],
+            [0, 1, 2]
+        ]
+        self.assertEqual(self.sol.updateMatrix(mat), expected)
 
     def test_case_4(self):
         mat = [
@@ -91,15 +110,20 @@ class TestUpdateMatrix(unittest.TestCase):
         ]
         self.assertEqual(self.sol.updateMatrix(mat), expected)
     
-    # def test_case_5(self):
-    #     mat = [
-    #         [1, 1, 0],
-    #         [1, 1, 1]
-    #     ]
-    #     expected = [
-    #         [2, 1, 0],
-    #         [3, 2, 1]
-    #     ]
+    def test_case_5(self):
+        mat = [
+            [1, 1, 0],
+            [1, 1, 1]
+        ]
+        expected = [
+            [2, 1, 0],
+            [3, 2, 1]
+        ]
+        self.assertEqual(self.sol.updateMatrix(mat), expected)
+        
+    # def test_case_6(self):
+    #     mat = [[1,0,1,1,0,0,1,0,0,1],[0,1,1,0,1,0,1,0,1,1],[0,0,1,0,1,0,0,1,0,0],[1,0,1,0,1,1,1,1,1,1],[0,1,0,1,1,0,0,0,0,1],[0,0,1,0,1,1,1,0,1,0],[0,1,0,1,0,1,0,0,1,1],[1,0,0,0,1,1,1,1,0,1],[1,1,1,1,1,1,1,0,1,0],[1,1,1,1,0,1,0,0,1,1]]
+    #     expected = [[1,0,1,1,0,0,1,0,0,1],[0,1,1,0,1,0,1,0,1,1],[0,0,1,0,1,0,0,1,0,0],[1,0,1,0,1,1,1,1,1,1],[0,1,0,1,1,0,0,0,0,1],[0,0,1,0,1,1,1,0,1,0],[0,1,0,1,0,1,0,0,1,1],[1,0,0,0,1,2,1,1,0,1],[2,1,1,1,1,2,1,0,1,0],[3,2,2,1,0,1,0,0,1,1]]
     #     self.assertEqual(self.sol.updateMatrix(mat), expected)
         
 if __name__ == '__main__':
