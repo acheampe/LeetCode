4a7aaaf2ee5a1786e586c2dae05109f970e9f74d
acheampe <acheampe@oregonstate.edu>
2025-10-07 09:16:02 -0400

    test 4 failing

--- neetCode/graphConcepts/shortest path/matrix_01BFS.py
+++ neetCode/graphConcepts/shortest path/matrix_01BFS.py
@@ -67,23 +67,23 @@ class TestUpdateMatrix(unittest.TestCase):
     def setUp(self):
         self.sol = Solution()
 
-    # def test_case_4(self):
-    #     mat = [
-    #         [1, 1, 0],
-    #     ]
-    #     expected = [
-    #         [2, 1, 0],
-    #     ]
-    #     self.assertEqual(self.sol.updateMatrix(mat), expected)
-        
-    def test_case_0(self):
+    def test_case_4(self):
         mat = [
-            [1, 0],
+            [1, 1, 0],
         ]
         expected = [
-            [1, 0],
+            [2, 1, 0],
         ]
         self.assertEqual(self.sol.updateMatrix(mat), expected)
+        
+    # def test_case_0(self):
+    #     mat = [
+    #         [1, 0],
+    #     ]
+    #     expected = [
+    #         [1, 0],
+    #     ]
+    #     self.assertEqual(self.sol.updateMatrix(mat), expected)
 
     # def test_case_1(self):
     #     mat = [
