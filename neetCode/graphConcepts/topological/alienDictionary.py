cf64f2f2aa00bdc8fb35fb65f1ffd827c2ac05a5
acheampe <acheampe@oregonstate.edu>
2025-11-21 15:30:41 -0500

    alienDictionary

--- neetCode/graphConcepts/topological/alienDictionary.py
+++ neetCode/graphConcepts/topological/alienDictionary.py
@@ -1,17 +1,62 @@
 import unittest
-from typing import List
 
 class Solution:
     def alienOrder(self, words: list[str]) -> str:
-        # <-- Implement this part
-        pass
+        
+        chr_graph: dict[str, set[str]] = {c: set() for word in words for c in word}
+        
+        for i in range(len(words) - 1):
+            first_word, second_word = words[i], words[i + 1]
+            min_length = min(len(first_word), len(second_word))
+            
+            for i in range(min_length):
+                if first_word[i] != second_word[i]:
+                    chr_graph[first_word[i]].add(second_word[i])
+                    break
+                
+                elif len(second_word) < len(first_word):
+                    return "" # not lexi ordered
+            
+        result = []
+        completed_path = set()
+        
+        def isCycle(c, curr_path):
+            
+            if c in curr_path:
+                return True # it is a cycle
+            
+            if c in completed_path:
+                return False # it is not a cycle
+            
+            curr_path.add(c)
+            
+            for next_c in chr_graph[c]:
+                cycle_detected = isCycle(next_c, curr_path) 
+                if cycle_detected:
+                    return True
+            
+            # post DFS
+            completed_path.add(c)
+            result.append(c)
+            
+            return False
+        
+        for chr in chr_graph.keys():
+            if chr in completed_path:
+                continue
+                
+            curr_path = set()
+            if isCycle(chr, curr_path):
+                return ""
+        
+        return ''.join(l for l in result[::-1])
 
 
 class TestAlienDictionary(unittest.TestCase):
     def setUp(self):
         self.sol = Solution()
 
-    def isValidOrder(self, order: str, words: List[str]) -> bool:
+    def isValidOrder(self, order: str, words: list[str]) -> bool:
         """Check if the given order is valid for the word list"""
         rank = {c: i for i, c in enumerate(order)}
         for i in range(len(words) - 1):
