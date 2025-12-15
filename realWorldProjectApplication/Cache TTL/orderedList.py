5984d0adfcafdf17b2ae96ada5bb5250f71a5ecc
acheampe <acheampe@oregonstate.edu>
2025-12-15 18:25:50 -0500

    cache

--- realWorldProjectApplication/Cache TTL/orderedList.py
+++ realWorldProjectApplication/Cache TTL/orderedList.py
@@ -1,6 +1,10 @@
 from collections import OrderedDict # combination of Linkedlist and deque data structure
 
 storage = OrderedDict()
+"""“An OrderedDict is implemented as a hash map that maps keys to nodes in a 
+doubly linked list. The hash map provides O(1) access to the node, and the linked 
+list provides O(1) reordering and eviction. Because we already have the node reference, 
+we never traverse the list.”"""
 
 storage['A'] = 12
 storage['B'] = 13
