79721c6f8d293788cfe9bd9b76cda186c1e7ba55
acheampe <acheampe@oregonstate.edu>
2025-12-17 11:13:07 -0500

    debugged URL shortener

--- realWorldProjectApplication/URL Shortener/url_shortener_debugging.py
+++ realWorldProjectApplication/URL Shortener/url_shortener_debugging.py
@@ -3,7 +3,7 @@ import string
 
 class URLShortener:
     def __init__(self, base_url="https://sho.rt/"):
-        self.base_url = base_url
+        self.base_url = base_url # mark as internal use only
         self.code_to_url = {}
 
     def _generate_code(self, length=6):
@@ -11,10 +11,12 @@ class URLShortener:
         return ''.join(random.choice(chars) for _ in range(length))
 
     def shorten(self, long_url):
-        code = self._generate_code()
+        code = self._generate_code() # to reduce collision and code space, perhaps using hashlib and base64 (just a representation) will be more efficient
+        # collision check needed to avoid accidental rewrites
         self.code_to_url[code] = long_url
         return self.base_url + code
 
     def resolve(self, short_url):
         code = short_url.split("/")[-1]
+        # need to implement gracefully handling keyError
         return self.code_to_url[code]
