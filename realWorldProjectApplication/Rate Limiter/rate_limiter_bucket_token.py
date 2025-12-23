33a7bbefb055d457ca9cfde8aa31a3b38058dbf3
acheampe <acheampe@oregonstate.edu>
2025-12-23 15:04:44 -0500

    rate_limiter_token

--- realWorldProjectApplication/Rate Limiter/rate_limiter_bucket_token.py
+++ realWorldProjectApplication/Rate Limiter/rate_limiter_bucket_token.py
@@ -1,6 +1,5 @@
 import time
-from typing import Any
-
+import typing
 
 class TokenBucketRateLimiter:
     def __init__(self, capacity: int, refill_rate: float):
@@ -8,32 +7,45 @@ class TokenBucketRateLimiter:
         capacity: max number of tokens in the bucket
         refill_rate: tokens added per second
         """
+        
         self.capacity = capacity
-        self.refill_rate = refill_rate
-        # key -> (tokens, last_refill_time)
-        self.buckets: dict[Any, tuple[float, float]] = {}
+        self.refill_rate: float = refill_rate
+        self._track_rate: dict[typing.Any, tuple[float, int]]= {}
+    
+    def is_valid_request(self, user_id: typing.Any):
+        
+        if not user_id:
+            return 'User_id not found'
+        fill_time = time.perf_counter()
 
-    def allow(self, key: Any) -> bool:
-        now = time.perf_counter()
-
-        if key not in self.buckets:
-            # initialize bucket as full
-            self.buckets[key] = (self.capacity - 1, now)
+        if user_id not in self._track_rate:
+            self._track_rate[user_id] = (fill_time, self.capacity)
+            
+        self.refill(user_id, fill_time)
+        
+        return self.is_allowed(user_id)
+    
+    def is_allowed(self, user_id: typing.Any) -> bool:
+        
+        last_filled, total_token = self._track_rate[user_id]
+        
+        if total_token > 0:
+            self._track_rate[user_id] = (last_filled, total_token - 1)
             return True
+        
+        return False
+    
+    def refill(self, user_id, curr_fill_time):
+        last_filled, total_token = self._track_rate[user_id]
 
-        tokens, last_refill = self.buckets[key]
+        elapsed = curr_fill_time - last_filled
+        to_add = int(elapsed * self.refill_rate)
 
-        # refill tokens based on elapsed time
-        elapsed = now - last_refill
-        refill = elapsed * self.refill_rate
-        tokens = min(self.capacity, tokens + refill)
+        if to_add > 0:
+            total_token = min(self.capacity, total_token + to_add)
+            last_filled = last_filled + (to_add / self.refill_rate)
 
-        if tokens < 1:
-            # no tokens available
-            self.buckets[key] = (tokens, now)
-            return False
-
-        # consume one token
-        tokens -= 1
-        self.buckets[key] = (tokens, now)
-        return True
+        self._track_rate[user_id] = (last_filled, total_token)
+        
+            
+        
